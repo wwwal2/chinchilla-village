9d4fc1396eb288df2029812e47b83595a43e81e6
wwwal2 <smalex9@gmail.com>
2020-09-29 11:00:58 +0300

    basic set up:
    add react, add typescript, add webpack options for dev and prod

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h4>
+        aaffddddssddd
+      </h4>
+    </div>
+  );
+};
---
+++ webpack.dev.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  devtool: "source-map",
+  
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json", ".less"],
+    alias: {
+      src: path.resolve(__dirname, "src"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      // {
+      //   test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      //   loader: "url-loader",
+      //   include : path.join(__dirname, 'images'),
+      //   options: {
+      //     limit: 10000,
+      //     name: "images/[name].[ext]",
+      //   },
+      // },
+      // {
+      //   test: /\.(png|svg|jpg|gif|ico)$/,
+      //   loader: 'file-loader',
+      //   options: {
+      //     name: '/images/[name].[ext]',
+      //   },
+      // },
+    ],
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    hotOnly: true,
+  },
+
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      title: "Chinchilla village",
+      template: require("html-webpack-template"),
+      bodyHtmlSnippet: "<div id=\"root\"></div>",
+      favicon: './src/images/favicon.ico',
+    }),
+  ],
+}
